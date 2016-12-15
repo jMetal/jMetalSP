818abfe02ce19a56f7b5366a0e9d6132c557a388
Antonio J. Nebro <antonio@lcc.uma.es>
2016-12-15 13:40:12 +0100

    Add file DynamicMOCellIT.java

---
+++ jmetalsp-algorithm/src/test/java/org/uma/jmetalsp/algorithm/DynamicMOCellIT.java
@@ -0,0 +1,22 @@
+package org.uma.jmetalsp.algorithm;
+
+
+import org.junit.Test;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetalsp.problem.DynamicProblem;
+
+/**
+ * Integration test for class DynamicMOCell
+ *
+ * @author Antonio J. Nebro <ajnebro@uma.es>
+ */
+public class DynamicMOCellIT {
+
+  @Test
+  public void shouldConstructorCreateTheAlgorithm() {
+    //DynamicProblem<?,?> problem = new FDA1() ;
+    //DynamicMOCell<DoubleSolution> dynamicMOCell = new DynamicMOCell<>()
+  }
+
+
+}
