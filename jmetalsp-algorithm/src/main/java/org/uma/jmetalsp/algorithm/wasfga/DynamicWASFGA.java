076f5e6f65e304b6894e7902371bcc783f06327a
cbarba <crisbarbag@gmail.com>
2017-05-05 14:35:41 +0200

    developing DynamicWASFGA

--- jmetalsp-algorithm/src/main/java/org/uma/jmetalsp/algorithm/wasfga/DynamicWASFGA.java
+++ jmetalsp-algorithm/src/main/java/org/uma/jmetalsp/algorithm/wasfga/DynamicWASFGA.java
@@ -1,7 +1,83 @@
 package org.uma.jmetalsp.algorithm.wasfga;
 
+import org.uma.jmetal.algorithm.multiobjective.wasfga.WASFGA;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.SolutionListUtils;
+import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
+import org.uma.jmetalsp.DynamicAlgorithm;
+import org.uma.jmetalsp.DynamicProblem;
+import org.uma.jmetalsp.observeddata.AlgorithmObservedData;
+import org.uma.jmetalsp.observer.Observable;
+
+import java.util.List;
+
 /**
- * Created by cris on 05/05/2017.
+ * @author Cristobal Barba <cbarba@lcc.uma.es>
  */
-public class DynamicWASFGA {
+public class DynamicWASFGA<S extends Solution<?>, O extends Observable<AlgorithmObservedData>>
+        extends WASFGA<S>
+        implements DynamicAlgorithm<List<S>, AlgorithmObservedData, Observable<AlgorithmObservedData>> {
+  private int completedIterations;
+  private boolean stopAtTheEndOfTheCurrentIteration = false;
+
+  O observable;
+
+  public DynamicWASFGA(Problem<S> problem, int populationSize, int maxIterations, CrossoverOperator<S> crossoverOperator, MutationOperator<S> mutationOperator, SelectionOperator<List<S>, S> selectionOperator, SolutionListEvaluator<S> evaluator, List<Double> referencePoint, O observable) {
+    super(problem, populationSize, maxIterations, crossoverOperator, mutationOperator, selectionOperator, evaluator, referencePoint);
+    completedIterations = 0;
+    this.observable = observable;
+  }
+
+  @Override
+  public DynamicProblem<S, ?> getDynamicProblem() {
+    return (DynamicProblem<S, ?>) super.getProblem();
+  }
+
+  @Override
+  public int getCompletedIterations() {
+    return completedIterations;
+  }
+
+  @Override
+  public void stopTheAlgorithm() {
+    stopAtTheEndOfTheCurrentIteration = true;
+  }
+
+  @Override
+  public void restart(int percentageOfSolutionsToRemove) {
+    SolutionListUtils.restart(getPopulation(), getDynamicProblem(), percentageOfSolutionsToRemove);
+    this.setPopulation(createInitialPopulation());
+    this.evaluatePopulation(this.getPopulation());
+    initProgress();
+  }
+
+  @Override
+  public Observable<AlgorithmObservedData> getObservable() {
+    return this.observable;
+  }
+
+  @Override
+  public String getName() {
+    return "DynamicWASFGA";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Dynamic version of algorithm WASFGA";
+  }
+
+  @Override
+  protected boolean isStoppingConditionReached() {
+    if (evaluations >= maxEvaluations) {
+      observable.setChanged();
+      observable.notifyObservers(new AlgorithmObservedData(getPopulation(), completedIterations));
+      restart(100);
+      completedIterations++;
+    }
+    return stopAtTheEndOfTheCurrentIteration;
+  }
 }
---
+++ jmetalsp-algorithm/src/main/java/org/uma/jmetalsp/algorithm/wasfga/DynamicWASFGABuilder.java
@@ -0,0 +1,18 @@
+package org.uma.jmetalsp.algorithm.wasfga;
+
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetalsp.DynamicProblem;
+import org.uma.jmetalsp.observeddata.AlgorithmObservedData;
+import org.uma.jmetalsp.observer.Observable;
+
+/**
+ * @author Cristobal Barba <cbarba@lcc.uma.es>
+ */
+public class DynamicWASFGABuilder <
+        S extends Solution<?>,
+        P extends DynamicProblem<S, ?>,
+        O extends Observable<AlgorithmObservedData>> {
+
+
+
+}
