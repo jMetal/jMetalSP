c43199d7fed6f86bd9aa9ef9d04e35163d1a26ae
cbarba <crisbarbag@gmail.com>
2018-06-01 09:05:17 +0200

    first commit

--- jmetalsp-spark/src/main/java/org/uma/jmetalsp/spark/SparkRuntime.java
+++ jmetalsp-spark/src/main/java/org/uma/jmetalsp/spark/SparkRuntime.java
@@ -30,6 +30,8 @@ public class SparkRuntime implements StreamingRuntime {
   private int duration ;
 
   public SparkRuntime(int duration) {
+   // SparkConf sparkConf = new SparkConf().setAppName("ReadJSon").setSparkHome("/Users/cbarba/Documents/tools/spark-2.3.0-bin-hadoop2.7").setMaster("local[2]");
+
     sparkConf = new SparkConf() ;
     this.duration = duration ;
     streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(this.duration)) ;
