dd9a13035360e138d2bacb8a1d6a3d34b80bf0b4
Antonio J. Nebro <antonio@lcc.uma.es>
2018-10-03 18:44:01 +0200

    Remove public declaration

--- jmetalsp-core/src/main/java/org/uma/jmetalsp/InteractiveAlgorithm.java
+++ jmetalsp-core/src/main/java/org/uma/jmetalsp/InteractiveAlgorithm.java
@@ -5,9 +5,9 @@ import org.uma.jmetalsp.util.restartstrategy.RestartStrategy;
 
 public interface InteractiveAlgorithm<S,R>  extends org.uma.jmetal.algorithm.InteractiveAlgorithm<S,R> {
 
-  public void restart(RestartStrategy restartStrategy);
-  public List<S> getPopulation();
-  public void compute();//List<S> population
-  public List<S> initializePopulation();
-  public void evaluate(List<S> population);
+  void restart(RestartStrategy restartStrategy);
+  List<S> getPopulation();
+  void compute();
+  List<S> initializePopulation();
+  void evaluate(List<S> population);
 }
