a23f85a59e8e7dcb6a92d161339df21bf69172ae
cbarba <crisbarbag@gmail.com>
2018-12-17 14:26:06 +0100

    DF4,DF5

--- jmetalsp-examples/src/main/java/org/uma/jmetalsp/examples/continuousproblemapplication/InDM2RunnerForContinuousProblems.java
+++ jmetalsp-examples/src/main/java/org/uma/jmetalsp/examples/continuousproblemapplication/InDM2RunnerForContinuousProblems.java
@@ -29,10 +29,7 @@ import org.uma.jmetalsp.impl.DefaultRuntime;
 import org.uma.jmetalsp.observeddata.AlgorithmObservedData;
 import org.uma.jmetalsp.observeddata.ObservedValue;
 import org.uma.jmetalsp.observer.impl.DefaultObservable;
-import org.uma.jmetalsp.problem.df.DF1;
-import org.uma.jmetalsp.problem.df.DF2;
-import org.uma.jmetalsp.problem.df.DF3;
-import org.uma.jmetalsp.problem.df.DF4;
+import org.uma.jmetalsp.problem.df.*;
 import org.uma.jmetalsp.problem.fda.FDA2;
 import org.uma.jmetalsp.util.restartstrategy.RestartStrategy;
 import org.uma.jmetalsp.util.restartstrategy.impl.CreateNRandomSolutions;
@@ -58,7 +55,7 @@ public class InDM2RunnerForContinuousProblems {
   public static void main(String[] args) throws IOException, InterruptedException {
     // STEP 1. Create the problem
     DynamicProblem<DoubleSolution, ObservedValue<Integer>> problem =
-            new DF4();
+            new DF5();
            // new FDA2();
 
     // STEP 2. Create and configure the algorithm
--- jmetalsp-problem/src/main/java/org/uma/jmetalsp/problem/df/DF5.java
+++ jmetalsp-problem/src/main/java/org/uma/jmetalsp/problem/df/DF5.java
@@ -10,27 +10,29 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DF4 extends DF implements Serializable {
-    public DF4(Observable<ObservedValue<Integer>> observable){
+public class DF5 extends DF implements Serializable {
+    public DF5(Observable<ObservedValue<Integer>> observable){
         this(10,2, observable);
     }
 
-    public DF4() {
+    public DF5() {
         this(new DefaultObservable<>()) ;
     }
 
-    public DF4(Integer numberOfVariables, Integer numberOfObjectives, Observable<ObservedValue<Integer>> observer) throws JMetalException {
+    public DF5(Integer numberOfVariables, Integer numberOfObjectives, Observable<ObservedValue<Integer>> observer) throws JMetalException {
         super(observer) ;
         setNumberOfVariables(numberOfVariables);
         setNumberOfObjectives(numberOfObjectives);
-        setName("DF4");
+        setName("DF5");
 
         List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables());
         List<Double> upperLimit = new ArrayList<>(getNumberOfVariables());
 
-        for (int i = 0; i < getNumberOfVariables(); i++) {
-            lowerLimit.add(-2.0);
-            upperLimit.add(2.0);
+        lowerLimit.add(0.0);
+        upperLimit.add(1.0);
+        for (int i = 1; i < getNumberOfVariables(); i++) {
+            lowerLimit.add(-1.0);
+            upperLimit.add(1.0);
         }
 
         setLowerLimit(lowerLimit);
@@ -51,28 +53,15 @@ public class DF4 extends DF implements Serializable {
     @Override
     public void evaluate(DoubleSolution solution) {
         double[] f = new double[getNumberOfObjectives()];
-        double a = Math.sin(0.5*Math.PI*time);
-        double b = 1.0d+ Math.abs(Math.cos(0.5*Math.PI*time));
-        double c = Math.max(Math.abs(a),a+b);
-        double H = 1.5+a;
-        double g = 1.0+evaluateG(solution, a,c);
+        double G = Math.sin(0.5*Math.PI*time);
+        int w = (int)Math.floor(10*G);
+        double g = 1.0d+helperSum(solution,1,solution.getNumberOfVariables(),G);
 
-
-        f[0] = g*Math.pow(Math.abs(solution.getVariableValue(0)-a),H);
-        f[1] = g*Math.pow(Math.abs(solution.getVariableValue(0)-a-b),H);
-       //if (Double.isNaN(f[1])){
-       //     f[1]=1.0;
-       //}
+        f[0] = g*(solution.getVariableValue(0)+0.02*Math.sin(w*Math.PI*solution.getVariableValue(0)))+2*time;
+        f[1] = g*(1-solution.getVariableValue(0)+0.02*Math.sin(w*Math.PI*solution.getVariableValue(0)))+2*time;
 
         solution.setObjective(0, f[0]);
         solution.setObjective(1, f[1]);
     }
 
-    private double evaluateG(DoubleSolution solution, double a, double c){
-        double result=0.0;
-        for (int i=1;i<solution.getNumberOfVariables();i++){
-            result+=Math.pow(solution.getVariableValue(i)-a*Math.pow(solution.getVariableValue(0)/c,2)/(i+1),2);
-        }
-        return result;
-    }
 }
